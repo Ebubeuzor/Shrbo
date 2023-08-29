f8cf7f1a43ef032838eab19334ce476acefe149a
DavidAjanaku <davidajanaku46163@gmail.com>
2023-08-29 17:28:22 +0100

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ListingInfoMain from './Views/ListingInfo/ListingInfoMain'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Views/Home";
+import ListingInfoMain from "./Views/ListingInfo/ListingInfoMain"
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <ListingInfoMain/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ListingInfoMain" element={<ListingInfoMain />} />
+
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
