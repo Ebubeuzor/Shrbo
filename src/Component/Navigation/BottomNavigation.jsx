ea21c672a1ecd1a1e611e34392cabd2fd18ca3e1
DavidAjanaku <davidajanaku46163@gmail.com>
2023-09-25 13:29:21 +0100

    David: make the bottom navigations have an active color

--- src/Component/Navigation/BottomNavigation.jsx
+++ src/Component/Navigation/BottomNavigation.jsx
@@ -1,30 +1,40 @@
 import React from 'react';
 import { FaSearch, FaHeart, FaSuitcase, FaInbox, FaUser } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function BottomNavigation() {
+  const location = useLocation();
+  const currentPage = location.pathname;
+
+  // Define the default color and active color for each navigation item
+  const defaultColor = 'white';
+  const activeColor = 'orange-400';
+
+  // Define a function to determine the color based on the current page
+  const getColor = (path) => (currentPage === path ? activeColor : defaultColor);
+
   return (
-    <div className=" md:hidden fixed bottom-0 left-0 w-full bg-gray-800 text-white py-4 flex justify-center z-50">
+    <div className="md:hidden fixed bottom-0 left-0 w-full bg-gray-800 text-white py-4 flex justify-center z-50">
       <div className="flex justify-between w-3/4">
         <Link to="/" className="cursor-pointer flex flex-col items-center">
-          <FaSearch className="text-2xl text-orange-400 mx-auto" />
-          <span className='text-[10px] text-orange-400'>Explore</span>
+          <FaSearch className={`text-2xl text-${getColor('/')}`} />
+          <span className={`text-[10px] text-${getColor('/')}`}>Explore</span>
         </Link>
         <Link to="/wishlist" className="cursor-pointer flex flex-col items-center">
-          <FaHeart className="text-2xl mx-auto" />
-          <span className='text-[10px]'>Wishlist</span>
+          <FaHeart className={`text-2xl text-${getColor('/wishlist')}`} />
+          <span className={`text-[10px] text-${getColor('/wishlist')}`}>Wishlist</span>
         </Link>
         <Link to="/trip" className="cursor-pointer flex flex-col items-center">
-          <FaSuitcase className="text-2xl mx-auto" />
-          <span className='text-[10px]'>Trips</span>
+          <FaSuitcase className={`text-2xl text-${getColor('/trip')}`} />
+          <span className={`text-[10px] text-${getColor('/trip')}`}>Trips</span>
         </Link>
         <Link to="/chat" className="cursor-pointer flex flex-col items-center">
-          <FaInbox className="text-2xl mx-auto" />
-          <span className='text-[10px]'>Inbox</span>
+          <FaInbox className={`text-2xl text-${getColor('/chat')}`} />
+          <span className={`text-[10px] text-${getColor('/chat')}`}>Inbox</span>
         </Link>
         <Link to="/profile" className="cursor-pointer flex flex-col items-center">
-          <FaUser className="text-2xl mx-auto" />
-          <span className='text-[10px]'>Profile</span>
+          <FaUser className={`text-2xl text-${getColor('/profile')}`} />
+          <span className={`text-[10px] text-${getColor('/profile')}`}>Profile</span>
         </Link>
       </div>
     </div>
