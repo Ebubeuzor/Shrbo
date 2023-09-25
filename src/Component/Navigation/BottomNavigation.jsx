80a581b927a6fa45adb8c17125d5f6869c876a61
DavidAjanaku <davidajanaku46163@gmail.com>
2023-09-25 11:41:56 +0100

    David: did the check date validation

--- src/Component/Navigation/BottomNavigation.jsx
+++ src/Component/Navigation/BottomNavigation.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function BottomNavigation() {
   return (
     <div className=" md:hidden fixed bottom-0 left-0 w-full bg-gray-800 text-white py-4 flex justify-center z-50">
-      <div className="flex space-x-8">
+      <div className="flex justify-between w-3/4">
         <Link to="/" className="cursor-pointer flex flex-col items-center">
           <FaSearch className="text-2xl text-orange-400 mx-auto" />
           <span className='text-[10px] text-orange-400'>Explore</span>
