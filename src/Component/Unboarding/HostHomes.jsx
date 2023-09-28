b26a7364d12adb861e8cef6380f343ecabcbf55e
DavidAjanaku <davidajanaku46163@gmail.com>
2023-09-28 14:04:56 +0100

    doing the unboarding

--- src/Component/Unboarding/HostHomes.jsx
+++ src/Component/Unboarding/HostHomes.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { FaHome, FaHotel, FaBed, FaBuilding } from "react-icons/fa";
 
 export default function HostHomes() {
+  const [selectedTypes, setSelectedTypes] = useState([]);
   const [step, setStep] = useState(0);
 
   const handleNext = () => {
@@ -11,6 +13,58 @@ export default function HostHomes() {
     setStep(step - 1);
   };
 
+  const propertyTypes = [
+    { id: "house", label: "House", icon: <FaHome /> },
+    { id: "hotel", label: "Hotel", icon: <FaHotel /> },
+    { id: "guestHouse", label: "Guest House", icon: <FaBed /> },
+    { id: "apartment", label: "Apartment", icon: <FaBuilding /> },
+  ];
+
+  const privacyTypes = [
+    {
+      id: "house",
+      label: "An entire place",
+      icon: <FaHome />,
+      description: "Guests have the whole place to themselves.",
+    },
+    {
+      id: "hotel",
+      label: "A room",
+      icon: <FaHotel />,
+      description: "Guests have the whole place to themselves.",
+    },
+
+    {
+      id: "guestHouse",
+      label: "A shared room",
+      icon: <FaBed />,
+      description: "Guests have the whole place to themselves.",
+    },
+  ];
+
+  const handleTypeSelection = (typeId) => {
+    setSelectedTypes([typeId]);
+  };
+
+  const addressFields = [
+    { id: "street", label: "Street Address" },
+    { id: "city", label: "City" },
+    { id: "state", label: "State" },
+    { id: "zipcode", label: "Zip Code" },
+  ];
+  
+  // Create state to store address information
+  const [address, setAddress] = useState({
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+  });
+  
+  // Function to handle address field changes
+  const handleAddressChange = (field, value) => {
+    setAddress({ ...address, [field]: value });
+  };
   const renderContent = () => {
     switch (step) {
       case 0:
@@ -83,8 +137,9 @@ export default function HostHomes() {
           </div>
         );
       case 1:
-        return <div className="mt-32 mx-auto  flex justify-center p-4">
-              <div className="  overflow-auto">
+        return (
+          <div className="mt-32 mx-auto  flex justify-center p-4">
+            <div className="  overflow-auto">
               <div className="md:flex md:justify-center md:flex-col md:mt-28 mb-20">
                 <h1 className="text-6xl">It’s easy to get started on Shbro</h1>
               </div>
@@ -101,21 +156,116 @@ export default function HostHomes() {
                         guests can stay.
                       </h3>
                     </div>
-                  
                   </div>
                   <div>
-                      <img
-                        className="w-64"
-                        src="https://a0.muscache.com/4ea/air/v2/pictures/da2e1a40-a92b-449e-8575-d8208cc5d409.jpg"
-                        alt=""
-                      />
-                    </div>
+                    <img
+                      className="w-64"
+                      src="https://a0.muscache.com/4ea/air/v2/pictures/da2e1a40-a92b-449e-8575-d8208cc5d409.jpg"
+                      alt=""
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-        </div>;
+          </div>
+        );
       case 2:
-        return <div>Final Onboarding Step 3</div>;
+        return (
+          <div className="mt-32 mx-auto  flex justify-center p-4">
+            <div className="  overflow-auto">
+              <div className="md:flex md:justify-center md:flex-col md:mt-28 mb-20">
+                <h1 className="text-6xl">
+                  Which of these best describes your place?
+                </h1>
+              </div>
+              <div className="pb-32">
+                <div className=" space-y-4">
+                  <h3 className="text-xl font-semibold">Property Types</h3>
+                  <div className="flex flex-wrap   w-full">
+                    {propertyTypes.map((type) => (
+                      <div
+                        key={type.id}
+                        className={`property-type h-20 w-32 m-3   flex ${
+                          selectedTypes.includes(type.id)
+                            ? "bg-orange-500 text-white"
+                            : "bg-gray-200 text-black"
+                        } px-4 py-2 rounded-md cursor-pointer flex-col justify-between`}
+                        onClick={() => handleTypeSelection(type.id)}
+                      >
+                        <span className="mr-2">{type.icon}</span>
+                        {type.label}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+
+      case 3:
+        return (
+          <div className="mt-32 mx-auto  flex justify-center p-4">
+            <div className="  overflow-auto">
+              <div className="md:flex md:justify-center md:flex-col md:mt-28 mb-20">
+                <h1 className="text-6xl">
+                  What type of place will guests have?
+                </h1>
+              </div>
+              <div className="pb-32">
+                <div className=" space-y-4">
+                  {/* <h3 className="text-xl font-semibold">Property Types</h3> */}
+                  <div className="  w-full">
+                    {privacyTypes.map((type) => (
+                      <div
+                        key={type.id}
+                        className={`property-type h-20  m-3   flex ${
+                          selectedTypes.includes(type.id)
+                            ? "bg-orange-500 text-white"
+                            : "bg-gray-200 text-black"
+                        } px-4 py-2 rounded-md cursor-pointer flex-col justify-between`}
+                        onClick={() => handleTypeSelection(type.id)}
+                      >
+                        <span className="mr-2">{type.icon}</span>
+                        {type.label}
+                        <p>{type.description}</p>
+                      </div>
+                      
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+
+        case 4:
+  return (
+    <div className="mt-32 mx-auto flex justify-center p-4">
+      <div className="overflow-auto">
+        <div className="md:flex md:justify-center md:flex-col md:mt-28 mb-20">
+          <h1 className="text-6xl">Enter your address</h1>
+        </div>
+        <div className="pb-32">
+          <div className="space-y-4">
+            {addressFields.map((field) => (
+              <div key={field.id} className="w-full">
+                <label className="text-xl font-semibold">{field.label}</label>
+                <input
+                  type="text"
+                  className="w-full border border-gray-300 rounded-md p-2"
+                  value={address[field.id]}
+                  onChange={(e) => handleAddressChange(field.id, e.target.value)}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      </div>
+  );
+
+        
       default:
         return null;
     }
@@ -125,8 +275,22 @@ export default function HostHomes() {
     <div>
       {renderContent()}
       <div className="bg-orange-400 flex fixed bottom-0 w-full text-center">
-        {step > 0 && <button onClick={handlePrevious} className="text-white  bg-orange-200 w-full p-4">Previous</button>}
-        {step < 2 && <button onClick={handleNext} className="text-white text-center  bg-orange-400 w-full p-4">Next</button>}
+        {step > 0 && (
+          <button
+            onClick={handlePrevious}
+            className="text-white  bg-orange-200 w-full p-4"
+          >
+            Previous
+          </button>
+        )}
+        {step < 4 && (
+          <button
+            onClick={handleNext}
+            className="text-white text-center  bg-orange-400 w-full p-4"
+          >
+            Next
+          </button>
+        )}
       </div>
     </div>
   );
