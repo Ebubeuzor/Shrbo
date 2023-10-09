eee6f3aa3ca47bb1a41628991cd67024edbbc51c
DavidAjanaku <davidajanaku46163@gmail.com>
2023-10-09 23:32:45 +0100

    adding the object for calender

--- src/Component/Dashboard/Schduler.jsx
+++ src/Component/Dashboard/Schduler.jsx
@@ -10,42 +10,62 @@ export default class Scheduler extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      blockingMode: false, // Track blocking/unblocking mode
       blockedDates: [], // Store blocked dates here
       selectedHouse: null, // Store the selected house details here
+      selectedDate: null, // Store the selected date
+      selectedDatePrice: "", // Store the price for the selected date
     };
   }
 
-  // Function to handle date blocking/unblocking
   handleDateClick = (dateInfo) => {
-    const { blockedDates } = this.state;
-    const blockedDate = dateInfo.dateStr;
-
-    // Check if the date is already blocked
-    if (blockedDates.includes(blockedDate)) {
-      // Date is blocked, remove it from the blockedDates array
-      const updatedBlockedDates = blockedDates.filter((date) => date !== blockedDate);
+    const { blockedDates, blockingMode } = this.state;
+    const clickedDate = dateInfo.dateStr;
+  
+    if (blockingMode) {
+      const updatedBlockedDates = blockedDates.includes(clickedDate)
+        ? blockedDates.filter((date) => date !== clickedDate)
+        : [...blockedDates, clickedDate];
+  
       this.setState({ blockedDates: updatedBlockedDates });
     } else {
-      // Date is not blocked, add it to the blockedDates array
-      this.setState({ blockedDates: [...blockedDates, blockedDate] });
+      // Check if the clicked date is blocked and unblock it
+      if (blockedDates.includes(clickedDate)) {
+        const updatedBlockedDates = blockedDates.filter((date) => date !== clickedDate);
+        this.setState({ blockedDates: updatedBlockedDates });
+      }
     }
   };
+  
 
-  // Function to handle house selection
+  handlePriceChange = (event) => {
+    const { selectedDate } = this.state;
+    const newPrice = event.target.value;
+
+    this.setState({ selectedDatePrice: newPrice });
+  };
+
+  handleBlockMode = () => {
+    this.setState({ blockingMode: true });
+  };
+
+  handleUnblockMode = () => {
+    this.setState({ blockingMode: false });
+  };
+
   handleHouseSelect = (house) => {
-    // Set the selected house
     this.setState({ selectedHouse: house });
   };
 
   render() {
-    const { blockedDates, selectedHouse } = this.state;
+    const { blockingMode, blockedDates, selectedHouse, selectedDate, selectedDatePrice } = this.state;
     const items = [
       {
         key: "1",
         label: (
           <div className="text-neutral-600 text-xl rounded-t-lg">Pricing</div>
         ),
-        children: <Pricing selectedHouse={selectedHouse} />, // Pass selectedHouse as a prop
+        children: <Pricing selectedHouse={selectedHouse} />,
       },
       {
         key: "2",
@@ -54,13 +74,10 @@ export default class Scheduler extends Component {
             Availability
           </div>
         ),
-        children: (
-          <div className="text-neutral-600 rounded-t-lg">Upcoming</div>
-        ),
+        children: <div className="text-neutral-600 rounded-t-lg">Upcoming</div>,
       },
     ];
-
-    // Define a list of houses
+  
     const apartments = [
       {
         name: "Lekki Admiralty",
@@ -94,19 +111,17 @@ export default class Scheduler extends Component {
       },
       // Add more houses and their pricing details as needed
     ];
-
-    // Extract house names from apartments
+  
     const houseOptions = apartments.map((apartment) => apartment.name);
-
+  
     return (
       <div className="flex box-border w-full">
         <div className="block flex-grow relative h-screen max-h-[800px] overflow-y-scroll example">
           <div className="flex flex-col relative py-8 px-6">
-            {/* Add a dropdown to select a house */}
             <select
               name="houseSelect"
               id="houseSelect"
-              onChange={(e) => this.handleHouseSelect(e.target.value)} 
+              onChange={(e) => this.handleHouseSelect(e.target.value)}
               className="py-4 border mb-4 pl-4"
             >
               <option value="">Select a house</option>
@@ -116,27 +131,59 @@ export default class Scheduler extends Component {
                 </option>
               ))}
             </select>
-            {selectedHouse && ( // Only render the calendar if an apartment is selected
+            {selectedHouse && ( // Only show the buttons when an apartment is selected
+              <div className="mb-4">
+                <button
+                  className={`${
+                    blockingMode ? "bg-red-500" : "bg-green-500"
+                  } text-white py-2 px-4 rounded mr-2`}
+                  onClick={this.handleBlockMode}
+                >
+                  Block
+                </button>
+                <button
+                  className={`${
+                    blockingMode ? "bg-green-500" : "bg-red-500"
+                  } text-white py-2 px-4 rounded`}
+                  onClick={this.handleUnblockMode}
+                >
+                  Unblock
+                </button>
+              </div>
+            )}
+            {selectedHouse && (
               <FullCalendar
                 plugins={[dayGridPlugin, interactionPlugin, multiMonthPlugin]}
                 initialView="dayGridMonth"
                 editable
                 selectable
-                dateClick={this.handleDateClick} // Handle date clicks to block/unblock dates
-                // Add blocked and unblocked dates to the calendar
+                dateClick={this.handleDateClick}
                 events={[
                   ...blockedDates.map((date) => ({
                     title: "Blocked",
                     start: date,
                     allDay: true,
                   })),
-                  ...this.getUnblockedDates(), // Include unblocked dates
+                  ...this.getUnblockedDates(),
                 ]}
               />
             )}
+            {selectedDate && blockingMode && (
+              <div className="mt-4 border">
+                <label className="font-semibold text-lg">
+                  Price for {selectedDate}
+                </label>
+                <input
+                  type="number"
+                  value={selectedDatePrice || ""}
+                  onChange={this.handlePriceChange}
+                  placeholder="Enter price per night"
+                />
+              </div>
+            )}
           </div>
         </div>
-
+  
         <section className="hidden w-[370px] border-l z-[1] min-[1128px]:block">
           <div className="max-h-[800px] block box-border overflow-auto h-screen relative bg-white">
             <div className="block box-border py-8 px-6">
@@ -147,14 +194,13 @@ export default class Scheduler extends Component {
       </div>
     );
   }
+  
 
-  // Helper function to get unblocked dates
   getUnblockedDates() {
     const { blockedDates } = this.state;
     const today = new Date();
     const unblockedDates = [];
 
-    // Iterate through future dates and add them if they are not blocked
     for (let i = 0; i < 365; i++) {
       const currentDate = new Date();
       currentDate.setDate(today.getDate() + i);
@@ -173,10 +219,7 @@ export default class Scheduler extends Component {
   }
 }
 
-
-
 const Pricing = ({ selectedHouse }) => {
-  // Define an array of apartment objects with pricing details for each house
   const apartments = [
     {
       name: "Lekki Admiralty",
@@ -211,144 +254,117 @@ const Pricing = ({ selectedHouse }) => {
     // Add more houses and their pricing details as needed
   ];
 
-  // Find the selected apartment/house based on the name
+
   const selectedApartment = apartments.find(
     (apartment) => apartment.name === selectedHouse
   );
 
   return (
-    <div className="block box-border overflow-y-scroll example ">
+    <div className="block box-border overflow-y-scroll example">
       {selectedApartment ? (
-        <>
-          {/* Display selected apartment/house details */}
-          <div className="block box-border mt-8 mb-8 min-[1128px]:mb-4">
-            <div className="box-border flex justify-between items-baseline mb-6">
-              <span>
-                <h2 className="m-0 p-0 text-2xl block box-border">
-                  <div className="min-[1128px]:text-lg font-semibold capitalize">
-                    {selectedHouse}
-                  </div>
-                </h2>
-              </span>
+        <div className="block box-border mt-8 mb-8 min-[1128px]:mb-4">
+          <div className="box-border flex justify-between items-baseline mb-6">
+            <span>
+              <h2 className="m-0 p-0 text-2xl block box-border">
+                <div className="min-[1128px]:text-lg font-semibold capitalize">
+                  {selectedHouse}
+                </div>
+              </h2>
+            </span>
 
-              <div className="block box-border uppercase font-semibold text-xs pr-[10px]">
-                <button className="bg-transparent cursor-pointer m-0 p-0 rounded-md underline">
-                  USD
-                </button>
+            <div className="block box-border uppercase font-semibold text-xs pr-[10px]">
+              <button className="bg-transparent cursor-pointer m-0 p-0 rounded-md underline">
+                USD
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 relative">
+            <div className="cursor-pointer w-full h-full outline-none">
+              <div className="pointer p-6 rounded-2xl border">
+                <div className="font-medium mb-2 mr-1 text-sm">Per night</div>
+                <div className="h-auto visible w-full">
+                  <div className="text-3xl break-keep inline-block font-extrabold">
+                    {selectedApartment.basePrice}
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="flex flex-col gap-4 relative">
-              {/* Render pricing details for the selected apartment/house */}
-              <div className="cursor-pointer w-full h-full outline-none">
-                <div className="pointer p-6 rounded-2xl border">
-                  <div className="font-medium mb-2 mr-1 text-sm">Per night</div>
-                  <div className="h-auto visible w-full">
-                    <div className="text-3xl break-keep inline-block font-extrabold">
-                      {selectedApartment.basePrice}
-                    </div>
+            <div className="rounded-2xl flex border p-6 justify-between gap-3">
+              <div className="touch-manipulation relative outline-none h-full w-full cursor-pointer">
+                <div className="mr-1 text-sm block box-border">
+                  Custom Weekend Price
+                </div>
+              </div>
+              <div className="block whitespace-nowrap -my-[10px]">
+                <div className="relative touch-manipulation cursor-pointer text-center p-[10px] -mx-[10px]">
+                  <div className="underline text-sm block box-border">
+                    {selectedApartment.customWeekendPrice}
                   </div>
                 </div>
               </div>
-
-              {/* Render other pricing details for the selected apartment/house */}
-              <div className="rounded-2xl flex border p-6 justify-between gap-3">
-                <div className="touch-manipulation relative outline-none h-full w-full cursor-pointer">
-                  <div className="mr-1 text-sm block box-border">
-                    Custom Weekend Price
-                  </div>
-                </div>
-                <div className="block whitespace-nowrap -my-[10px]">
-                  <div className="relative touch-manipulation cursor-pointer text-center p-[10px] -mx-[10px]">
-                    <div className="underline text-sm block box-border">
-                      {selectedApartment.customWeekendPrice}
+            </div>
+            <div className="">
+              <form>
+                <span className="px-6 py-5 border rounded-2xl flex items-center justify-between">
+                  <div className="text-sm block box-border">Smart Pricing</div>
+                  <label className="switch">
+                    <input type="checkbox" name="" />
+                    <span className="slider round"></span>
+                  </label>
+                </span>
+              </form>
+            </div>
+            <div className="block box-border mt-8 mb-8 min-[1128px]:mb-4">
+              <div className="box-border flex justify-between items-baseline mb-6">
+                <span>
+                  <h2 className="m-0 p-0 text-2xl block box-border">
+                    <div className="min-[1128px]:text-lg font-semibold capitalize">
+                      Discounts
                     </div>
-                  </div>
-                </div>
+                  </h2>
+                  <div className=" ">Adjust your pricing to add more guests.</div>
+                </span>
               </div>
-
-              <div className="">
-                <form>
-                  <span className="px-6 py-5 border rounded-2xl flex items-center justify-between">
-                    <div className="text-sm block box-border">
-                      Smart Pricing
-                    </div>
-                    <label className="switch">
-                      <input type="checkbox" name="" />
-                      <span className="slider round"></span>
-                    </label>
-                  </span>
-                </form>
-              </div>
-
-              <div className="block box-border mt-8 mb-8 min-[1128px]:mb-4">
-                <div className="box-border flex justify-between items-baseline mb-6">
-                  <span>
-                    <h2 className="m-0 p-0 text-2xl block box-border">
-                      <div className="min-[1128px]:text-lg font-semibold capitalize">
-                        Discounts
+              <div className="flex flex-col gap-4 relative">
+                <div className="cursor-pointer w-full h-full outline-none">
+                  <div className="pointer p-6 rounded-2xl border">
+                    <div className="mb-1 font-medium mr-1 text-sm">Weekly</div>
+                    <div className="mb-3 mr-1 text-sm">for 7 nights or more</div>
+                    <div className="h-auto visible w-full flex justify-between items-baseline">
+                      <div className="text-3xl break-keep inline-block font-extrabold">
+                        {selectedApartment.weeklyDiscount}
                       </div>
-                    </h2>
-                    <div className=" ">
-                      Adjust your pricing to add more guests.
+                      <div className="break-keep inline-block text-slate-600 text-sm">
+                        Weekly average is {selectedApartment.weeklyAverage}
+                      </div>
                     </div>
-                  </span>
+                  </div>
                 </div>
-                <div className="flex flex-col gap-4 relative">
-                  {/* Render weekly discount */}
-                  <div className="cursor-pointer w-full h-full outline-none">
-                    <div className="pointer p-6 rounded-2xl border">
-                      <div className="mb-1 font-medium mr-1 text-sm">
-                        Weekly
+                <div className="cursor-pointer w-full h-full outline-none">
+                  <div className="pointer p-6 rounded-2xl border">
+                    <div className="mb-1 font-medium mr-1 text-sm">Monthly</div>
+                    <div className="mb-3 mr-1 text-sm">for 28 nights or more</div>
+                    <div className="h-auto visible w-full flex justify-between items-baseline">
+                      <div className="text-3xl break-keep inline-block font-extrabold">
+                        {selectedApartment.monthlyDiscount}
                       </div>
-                      <div className="mb-3 mr-1 text-sm">
-                        for 7 nights or more
-                      </div>
-                      <div className="h-auto visible w-full flex justify-between items-baseline">
-                        <div className="text-3xl break-keep inline-block font-extrabold">
-                          {selectedApartment.weeklyDiscount}
-                        </div>
-                        <div className="break-keep inline-block text-slate-600 text-sm">
-                          Weekly average is {selectedApartment.weeklyAverage}
-                        </div>
+                      <div className="break-keep inline-block text-sm text-slate-600">
+                        Monthly average is {selectedApartment.monthlyAverage}
                       </div>
                     </div>
                   </div>
-
-                  {/* Render monthly discount */}
-                  <div className="cursor-pointer w-full h-full outline-none">
-                    <div className="pointer p-6 rounded-2xl border">
-                      <div className="mb-1 font-medium mr-1 text-sm">
-                        Monthly
-                      </div>
-                      <div className="mb-3 mr-1 text-sm">
-                        for 28 nights or more
-                      </div>
-                      <div className="h-auto visible w-full flex justify-between items-baseline">
-                        <div className="text-3xl break-keep inline-block font-extrabold">
-                          {selectedApartment.monthlyDiscount}
-                        </div>
-                        <div className="break-keep inline-block text-sm text-slate-600">
-                          Monthly average is {selectedApartment.monthlyAverage}
-                        </div>
-                      </div>
+                </div>
+                <div className="rounded-2xl flex border p-6 justify-between items-center gap-3">
+                  <div className="touch-manipulation relative outline-none h-full w-full cursor-pointer">
+                    <div className="mr-1 text-sm block box-border">More Discounts</div>
+                    <div className="mr-1 text-sm block box-border">
+                      {selectedApartment.moreDiscounts}
                     </div>
                   </div>
-
-                  {/* Render more discounts */}
-                  <div className="rounded-2xl flex border p-6 justify-between items-center gap-3">
-                    <div className="touch-manipulation relative outline-none h-full w-full cursor-pointer">
-                      <div className="mr-1 text-sm block box-border">
-                        More Discounts
-                      </div>
-                      <div className="mr-1 text-sm block box-border">
-                        {selectedApartment.moreDiscounts}
-                      </div>
-                    </div>
-                    <div className="block whitespace-nowrap -my-[10px]">
-                      <div className="relative touch-manipulation cursor-pointer text-center p-[10px] -mx-[10px]">
-                        <div className="text-sm block box-border">
-                          <IoIosArrowForward />
-                        </div>
+                  <div className="block whitespace-nowrap -my-[10px]">
+                    <div className="relative touch-manipulation cursor-pointer text-center p-[10px] -mx-[10px]">
+                      <div className="text-sm block box-border">
+                        <IoIosArrowForward />
                       </div>
                     </div>
                   </div>
@@ -356,7 +372,7 @@ const Pricing = ({ selectedHouse }) => {
               </div>
             </div>
           </div>
-        </>
+        </div>
       ) : (
         <div>Select an apartment/house to view details</div>
       )}
